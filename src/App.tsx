7fe08fa2f19201783a864690f5b6769c70d8961e
Max de Vocht <maxdevocht@hotmail.com>
2024-08-08 14:16:15 +0200

    Update

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import SkillSection from "./sections/SkillSection";
+import DockSection from "./sections/DockSection";
+
+function App() {
+  return (
+    <>
+      <SkillSection />
+      <DockSection />
+    </>
+  );
+}
+
+export default App;
