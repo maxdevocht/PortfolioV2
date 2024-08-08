d23b136f98e0eb88be3015c2129b6e1e07c98ced
Max de Vocht <maxdevocht@hotmail.com>
2024-08-08 15:31:21 +0200

    Setup Skill section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import SkillSection from "./sections/SkillSection";
 import DockSection from "./sections/DockSection";
+import { cn } from "./lib/utils";
+import DotPattern from "./components/magicui/dot-pattern";
 
 function App() {
   return (
-    <>
-      <SkillSection />
-      <DockSection />
-    </>
+    <div className="relative w-full min-h-screen overflow-hidden">
+      {/* Background DotPattern */}
+      <DotPattern
+        className={cn(
+          "absolute inset-0 z-0 [mask-image:radial-gradient(300px_circle_at_center,white,transparent)]"
+        )}
+      />
+
+      {/* Main content */}
+      <div className="relative z-10">
+        <SkillSection />
+        <DockSection />
+      </div>
+    </div>
   );
 }
 
